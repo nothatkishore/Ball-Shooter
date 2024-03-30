5ee5b0f45a45f131d3be1637c09548dd495fa32f
kishoear <kishorear.programming@gmail.com>
2024-03-30 23:18:55 +0530

    Enemies working

--- Classic Version/script.js
+++ Classic Version/script.js
@@ -2,13 +2,12 @@ const canvas = document.querySelector('canvas');
 
 const context = canvas.getContext('2d');
 
-
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-class Player
+class Player 
 {
-    constructor(radius, color)
+    constructor() 
     {
         this.x = canvas.width / 2;
         this.y = canvas.height / 2;
@@ -16,21 +15,21 @@ class Player
         this.color = 'blue';
     }
 
-    draw()
+    draw() 
     {
-        context.beginPath();    //This is used before drawing any shapes
-        context.arc(this.x, this.y, this.radius, 0, 2*Math.PI, false); //This is used to draw circles
-        context.fillStyle = this.color;  //This is used to choose fill color
-        context.fill(); //This is used to fill the shape will specified color
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        context.fillStyle = this.color;
+        context.fill();
     }
 }
 
-const p1 = new Player(); //This is an object for player
+const p1 = new Player();
 p1.draw();
 
-class Projectile
+class Projectile 
 {
-    constructor(x, y, velocity)
+    constructor(x, y, velocity) 
     {
         this.x = x;
         this.y = y;
@@ -39,7 +38,7 @@ class Projectile
         this.velocity = velocity;
     }
 
-    draw()
+    draw() 
     {
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
@@ -47,85 +46,98 @@ class Projectile
         context.fill();
     }
 
-    update()    //This function is used to make the circle move with particular velocity.
+    update() 
     {
         this.draw();
-        this.x = this.x + this.velocity.x;
-        this.y = this.y + this.velocity.y;
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
     }
-
 }
 
-
-class Enemy
+class Enemy 
 {
-    constructor(x, y, radius, color, velocity)
+    constructor(x, y, radius, velocity) 
     {
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.velocity = velocity;
+        this.color = 'green';
     }
 
-    draw()
+    draw() 
     {
         context.beginPath();
-        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        context.fillStyle = 'green';
+        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        context.fillStyle = this.color;
         context.fill();
     }
 
-    update()
+    update() 
     {
         this.draw();
-        this.x = this.x + this.velocity.x;
-        this.y = this.y + this.velocity.y;
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
     }
-
 }
 
-const enemies = []
+const enemies = [];
 
-function spawn_enemies()
+function spawn_enemies() 
 {
-    setInterval(() =>
+    setInterval(() => 
     {
-        const angle = Math.atan2(canvas.height/2 - y, canvas.width/2 - x);
-        const velocity = {x : Math.cos(angle), y : Math.sin(angle)};
-        const enemy = new Enemy(100, 100, 30, 'green', velocity);
+        const radius = Math.random() * 30 ;
+
+        let x, y;
+
+        if (Math.random() < 0.5) 
+        {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+            y = Math.random() * canvas.height;
+        } 
+        
+        else 
+        {
+            x = Math.random() * canvas.width;
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+        }
+
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+        const velocity = { x: Math.cos(angle), y: Math.sin(angle) };
+
+        const enemy = new Enemy(x, y, radius, velocity);
+
         enemies.push(enemy);
-    }, 1000)
+        console.log(enemy);
+    }, 1000);
 }
 
-const projectiles = []; //This list contains set of projectile points need to be targeted
+const projectiles = [];
 
-function animate()  //This function is used to animate the process
+function animate() 
 {
     requestAnimationFrame(animate);
     context.clearRect(0, 0, canvas.width, canvas.height);
-    p1.draw(); //This is used to draw the player
-    projectiles.forEach((projectile) =>
+    p1.draw();
+    projectiles.forEach((projectile) => 
     {
         projectile.update();
-    })
+    });
 
-    enemies.forEach((enemy) =>
+    enemies.forEach((enemy) => 
     {
         enemy.update();
     });
 }
 
 addEventListener('click', (event) => 
-{  
-    const angle = Math.atan2((event.clientY - canvas.height/2), (event.clientX - canvas.width/2));
-    const velocity = {x : Math.cos(angle), y : Math.sin(angle)};
-    // const velocity = {x : event.clientX - canvas.width/2, y : event.clientY - canvas.height/2};
-    const projectile = new Projectile(canvas.width/2, canvas.height/2, velocity);
+{
+    const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
+    const velocity = { x: Math.cos(angle), y: Math.sin(angle) };
+    const projectile = new Projectile(canvas.width / 2, canvas.height / 2, velocity);
     projectiles.push(projectile);
+});
 
-}); 
-animate(); 
 spawn_enemies();
-
-
-
+animate();
